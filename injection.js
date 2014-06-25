2b36e7f4f28d248b1c767cc882680340d9a4288d
zino <jinho.bang@samsung.com>
2014-06-25 22:27:06 +0900

    Fix some bugs of injection script.
    Fix some bugs of injection script.

--- injection.js
+++ injection.js
@@ -2,20 +2,18 @@ fixedLayout(__layoutWidth__);
 
 function fixedLayout(layoutWidth)
 {
-  document.body.style = document.body.style + "overflow:hidden";
   var meta = document.querySelector("meta[name=viewport]");
 
-  if (meta != undefined && meta != null) {
+  if (meta != undefined && meta != null)
     return;
-
-    // force fixed layout mode
-    document.head.removeChild(meta);
-  }
 
   meta = document.createElement("meta");
   meta.setAttribute("name", "viewport");
 
-  var deviceWidth = document.body.clientWidth;
+  // FIXME: We should calculate scrollBarWidth.
+  // For now, we can use the following constant.
+  const SCROLL_BAR_WIDTH = 25;
+  var deviceWidth = document.body.clientWidth - SCROLL_BAR_WIDTH;
   var scaleFactor = deviceWidth / layoutWidth;
   var queryString = "width=" + layoutWidth + ",minimum-scale=" +
       scaleFactor + ",maximum-scale=" + scaleFactor;
